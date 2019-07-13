d2b846ca7e44fba2bb0972abd7d16d2210daa9fb
Satria Bayuaji <satria_26@apps.ipb.ac.id>
2019-07-13 10:32:42 +0700

    Adding files project

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1f, jumpForce = 700f;
+
+    [SerializeField]
+    private GameObject[] Chara = new GameObject[2];
+
+    [SerializeField]
+    private string charColor;
+
+
+    public bool moveRight, moveLeft, jump, change;
+    public int arrayIndex = 0, cnt = 0;
+    
+    Rigidbody2D rb;
+
+    void Awake () {
+        Chara[arrayIndex % 2].SetActive(true);
+        charColor = Chara[arrayIndex % 2].tag;
+    }
+
+    // Start is called before the first frame update
+    void Start () {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    void FixedUpdate () {
+
+        if (moveRight)
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+
+        if (moveLeft)
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+
+        if (jump) {
+            PlayerJump();
+        }
+
+        if (change)
+        {
+            CharChange();
+        }
+    }
+
+    public void PlayerJump () {
+        if (rb.velocity.y == 0)
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
+    }
+
+    public void CharChange() {
+        if (cnt < 1){
+            Chara[arrayIndex % 2].SetActive(false);
+            arrayIndex++;
+            charColor = Chara[arrayIndex % 2].tag;
+            Chara[arrayIndex % 2].SetActive(true);
+            cnt++;
+        }
+    }
+
+} //Class
---
+++ Assets/Script/Touch.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour
+{
+    private PlayerMove player;
+    public Rigidbody2D pemain;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMove>();
+    }
+
+    public void LeftArrow() {
+        player.moveLeft = true;
+        player.moveRight = false;
+    }
+
+    public void RightArrow()
+    {
+        player.moveLeft = false;
+        player.moveRight = true;
+    }
+
+    public void ReleaseLeftArrow()
+    {
+        player.moveLeft = false;
+        pemain.velocity = new Vector2(0, pemain.velocity.y);
+    }
+
+    public void ReleaseRightArrow()
+    {
+        player.moveRight = false;
+        pemain.velocity = new Vector2(0, pemain.velocity.y);
+    }
+
+    public void JumpButton () {
+        player.jump = true;
+    }
+
+    public void ReleaseJumpButton() {
+        player.jump = false;
+    }
+
+    public void changeButton()
+    {
+        player.change = true;
+    }
+
+    public void releaseChangeButton()
+    {
+        player.change = false;
+        player.cnt = 0;
+    }
+}
